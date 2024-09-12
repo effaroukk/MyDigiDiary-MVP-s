286782b2f9bea6c76d2daf6e1e78a96f791c0031
Umar Farouk Abdulai <138718476+effaroukk@users.noreply.github.com>
2024-09-12 00:15:36 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import App from './App';
 import './styles/App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root for React 18
 root.render(<App />);
