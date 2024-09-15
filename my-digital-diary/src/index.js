be019e6f054ae5f9faf0bf45d1c93ed4b4407c6f
Umar Farouk Abdulai <138718476+effaroukk@users.noreply.github.com>
2024-09-15 02:18:10 +0000

    Update index.js

--- my-digital-diary/src/index.js
+++ my-digital-diary/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './A6-Notebook.png'; // Adjust the path if necessary
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
